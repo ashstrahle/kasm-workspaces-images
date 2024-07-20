625128822b7ccd9678dde2177557e952bcf2c227
Ashley Strahle <a.strahle@me.com>
2024-07-20 15:13:32 +1000

    bugfix

--- src/ubuntu/install/armcord/install_armcord.sh
+++ src/ubuntu/install/armcord/install_armcord.sh
@@ -8,13 +8,15 @@ tar xzvf armcord.tar.gz
 mv ArmCord-$ARMCORDVER-arm64 /opt/ArmCord
 rm armcord.tar.gz
 
+# Cleanup
+if [ -z ${SKIP_CLEAN+x} ]; then
+    apt-get autoclean
+    rm -rf \
+        /var/lib/apt/lists/* \
+        /var/tmp/* \
+        /tmp/*
+fi
+
 # Cleanup for app layer
 chown -R 1000:0 $HOME
 find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
-if [ -z ${SKIP_CLEAN+x} ]; then
-  apt-get autoclean
-  rm -rf \
-    /var/lib/apt/lists/* \
-    /var/tmp/* \
-    /tmp/*
-fi

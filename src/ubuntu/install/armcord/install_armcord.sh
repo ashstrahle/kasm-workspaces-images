bb60ea7c7cbcca529ecf9ee87d37da7f41b51df8
Ashley Strahle <a.strahle@me.com>
2024-07-20 15:45:05 +1000

    latest armcord from .deb

--- src/ubuntu/install/armcord/install_armcord.sh
+++ src/ubuntu/install/armcord/install_armcord.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 set -ex
 
-ARMCORDVER=3.2.7
-# Install Armcord
-wget -qO armcord.tar.gz https://github.com/ArmCord/ArmCord/releases/download/v$ARMCORDVER/ArmCord-$ARMCORDVER-arm64.tar.gz
-tar xzvf armcord.tar.gz
-mv ArmCord-$ARMCORDVER-arm64 /opt/ArmCord
-rm armcord.tar.gz
+# Install Armcord from deb
+OWNER="ArmCord"
+REPO="ArmCord"
+
+# Fetch the latest release information using GitHub API
+LATEST_RELEASE=$(curl -s "https://api.github.com/REPOs/$OWNER/$REPO/releases/latest")
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found. Please install it to parse JSON."
+    exit 1
+fi
+
+# Extract the URL of the latest ARM64 .deb file
+DEB_URL=$(echo "$LATEST_RELEASE" | jq -r '.assets[] | select(.name | endswith("_arm64.deb")) | .browser_download_url')
+
+# Check if the .deb file URL was found
+if [ -z "$DEB_URL" ]; then
+    echo "No ARM64 .deb file found in the latest release."
+    exit 1
+fi
+curl -L -o ArmCord-latest-arm64.deb "$DEB_URL"
+apt-get update
+apt-get install -y ./ArmCord-latest-arm64.deb
+rm ArmCord-latest-arm64.deb
 
 # Cleanup
 if [ -z ${SKIP_CLEAN+x} ]; then
